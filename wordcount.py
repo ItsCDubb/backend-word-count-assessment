bf687310e21f9c0b4a98ccc69eee7b7a30cafd83
Chris Warren <chris@chriswarren.dev>
2020-05-29 13:01:16 -0500

    Updated wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -35,9 +35,7 @@ def create_word_dict(filename):
     Returns a word_count dict for the given file.
     """
     word_count={}
-
     with open(filename) as f:
-
         for line in f:
             words = line.split()
             for word in words:
@@ -74,16 +72,13 @@ def main(args):
     if len(args) != 2:
         print('usage: python wordcount.py {--count | --topcount} file')
         sys.exit(1)
-
     option = args[0]
     filename = args[1]
-
     if option == '--count':
         print_words(filename)
     elif option == '--topcount':
         print_top(filename)
     else:
         print('unknown option: ' + option)
-
 if __name__ == '__main__':
     main(sys.argv[1:])
